9f9904c3f08c04f6e6fbd15252d46bc13f23e966
Tareq-dev <tareque.dev@gmail.com>
2023-02-19 22:34:35 +0600

    added server

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+
+// user - tareq
+//pass - STVMbgNLHprbExuc
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://tareq:STVMbgNLHprbExuc@cluster0.zw6y76r.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const postCollection = client.db("pingpong").collection("posts");
+
+    app.get("/posts", async (req, res) => {
+      const query = {};
+      const cursor = postCollection.find(query);
+      const posts = await cursor.toArray();
+      res.send(posts);
+    });
+
+    // app.get("/service/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const service = await serviceCollection.findOne(query);
+    //   res.send(service);
+    // });
+
+    // //POST
+
+    // app.post("/service", async (req, res) => {
+    //   const newService = req.body;
+    //   const result = await serviceCollection.insertOne(newService);
+    //   res.send(result);
+    // });
+
+    // // DELETE
+
+    // app.delete("/service/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await serviceCollection.deleteOne(query);
+    //   res.send(result);
+    // });
+
+    // //order collection
+    // app.get("/order", async (req, res) => {
+    //   const email = req.query.email;
+    //   if (email) {
+    //     const query = { email: email };
+    //     const cursor = orderCollection.find(query);
+    //     const orders = await cursor.toArray();
+    //     res.send(orders);
+    //   } else {
+    //     res.status(403).send({ message: "Forbidden access" });
+    //   }
+    // });
+
+    // app.post("/order", async (req, res) => {
+    //   const order = req.body;
+    //   const result = await orderCollection.insertOne(order);
+    //   res.send(result);
+    // });
+
+    // //auth JWT
+    // app.post("/login", async (req, res) => {
+    //   const user = req.body;
+    //   const accessToken = jwt.sign(user, process.env.ACCESS_KEY_ID, {
+    //     expiresIn: "1d",
+    //   });
+    //   res.send(accessToken);
+    // });
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running pingpong");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
